255fc6c86b54aa5af1a5c4a223e26856035a58e3
Simone Celia <simon387@hotmail.it>
2018-07-17 11:41:46 +0200

    test

---
+++ StructuralPatterns/src/main/java/decorator/LuxuryCar.java
@@ -0,0 +1,14 @@
+package decorator;
+
+public class LuxuryCar extends CarDecorator {
+
+	public LuxuryCar(Car car) {
+		super(car);
+	}
+
+	@Override
+	public void assemble() {
+		super.assemble();
+		System.out.println(" Adding features of Luxury Car.");
+	}
+}
